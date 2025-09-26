f96966effa7d6decdee570e961cf3cf077b1961d
Ala Hassine <contact.ala.hassine@gmail.com>
2025-09-26 21:56:20 +0100

    Add SetBit Header File

---
+++ src/SetBit.h
@@ -0,0 +1,8 @@
+#ifndef SETBIT_H_INCLUDED
+#define SETBIT_H_INCLUDED
+
+int ToggleBit(int num, int bitPos);
+int ClearBit(int num, int bitPos);
+int SetBit(int num, int bitPos);
+
+#endif // SETBIT_H_INCLUDED
